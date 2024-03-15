8f5c1d6a9c449ab76a93cbdd2bbb5fd82b260125
Mikhail Pazniak <mikhail.pazniak@itechart-group.com>
2024-03-15 11:59:46 +0300

    v1.1.0: Added AWS SSM Parameter Store - get SSM PS parameters by prefix

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const core = require('@actions/core');
 // const github = require('@actions/github');
 const { SecretsManagerClient, ListSecretsCommand, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
+const { SSMClient, DescribeParametersCommand } = require("@aws-sdk/client-ssm");
+const awsRegion = core.getInput('aws-default-region');
 
 let secretsPrefix = '';
 try {
     process.env.AWS_ACCESS_KEY_ID = core.getInput('aws-access-key-id');
     process.env.AWS_SECRET_ACCESS_KEY = core.getInput('aws-secret-access-key');
-    process.env.AWS_DEFAULT_REGION = core.getInput('aws-default-region');
+    process.env.AWS_DEFAULT_REGION = awsRegion;
 } catch (error) {
     core.setFailed(error.message);
     console.error(error.message);
@@ -19,7 +21,8 @@ try {
     console.error(error.message);
 }
 
-const client = new SecretsManagerClient({ region: core.getInput('aws-default-region') });
+const client = new SecretsManagerClient({ region: awsRegion });
+const ssmClient = new SSMClient({ region: awsRegion });
 
 const getSecrets = async (secretsPrefix) => {
     let response;
@@ -37,12 +40,14 @@ const getSecrets = async (secretsPrefix) => {
             },
         ]
     };
+
     try {
         const command = new ListSecretsCommand(input);
         response = await client.send(command);
     } catch (error) {
         core.setFailed(error.message);
     }
+
     const pagedSecrets = response.SecretList.slice(0);
     input.NextToken = response.NextToken;
     while (input.NextToken !== undefined) {
@@ -58,6 +63,7 @@ const getSecrets = async (secretsPrefix) => {
             core.setFailed(`Error 'List Secrets': ${error}\n${error.stack}`);
         }
     }
+
     for (const secret of pagedSecrets) {
         if (secret.ARN && secret.Name) {
             allSecrets.push({
@@ -69,9 +75,59 @@ const getSecrets = async (secretsPrefix) => {
     return allSecrets;
 }
 
+const getParameters = async (secretsPrefix) => {
+    let response;
+    const allParameters = [];
+    const input = {
+        MaxResults: 10,
+        NextToken: undefined,
+        ParameterFilters: [
+            {
+                Key: 'Name',
+                Option: 'Contains',
+                Values: [
+                    secretsPrefix
+                ]
+            },
+        ]
+    };
+
+    try {
+        const command = new DescribeParametersCommand(input);
+        response = await ssmClient.send(command);
+    } catch (error) {
+        core.setFailed(`Error 'DescribeParameters': ${error}\n${error.stack}`);
+    }
+
+    const pagedParameters = response.Parameters.slice(0);
+    input.NextToken = response.NextToken;
+    while (input.NextToken !== undefined) {
+        try {
+            const command = new DescribeParametersCommand(input);
+            const response = await ssmClient.send(command);
+            input.NextToken = response.NextToken;
+            const paramsData = response.Parameters;
+            paramsData.map(async item => {
+                pagedParameters.push(item);
+            });
+        } catch (error) {
+            core.setFailed(`Error 'Next page DescribeParameters': ${error}\n${error.stack}`);
+        }
+    }
+
+    for (const parameter of pagedParameters) {
+        if (parameter.ARN && parameter.Name) {
+            allParameters.push({
+                name: parameter.Name.replace('/' + secretsPrefix, '').toUpperCase(),
+                valueFrom: parameter.ARN
+            });
+        }
+    }
+    return allParameters;
+}
+
 const getSecretValue = async (secretsName) => {
     const secretItem = [];
-    const client = new SecretsManagerClient({ region: core.getInput('aws-default-region') });
     const input = {
         SecretId: secretsName
     };
@@ -86,7 +142,7 @@ const getSecretValue = async (secretsName) => {
                     valueFrom: `${response.ARN}:${key}::`
                 });
             }
-        } catch(error) {
+        } catch (error) {
             secretItem.push({
                 name: response.Name.replace(secretsPrefix, '').replace(/\//g, '_').toUpperCase(),
                 valueFrom: response.ARN
@@ -101,9 +157,13 @@ const handleSecrets = async (secretsPrefix) => {
     const secretsJSONlist = [];
     try {
         const secretsList = await getSecrets(secretsPrefix);
+        const parametersList = await getParameters(secretsPrefix);
         for (const secret of secretsList) {
             const newItem = await getSecretValue(secret.name);
             newItem.map(item => secretsJSONlist.push(item));
+        }
+        for (const parameter of parametersList) {
+            secretsJSONlist.push(parameter);
         }
         core.setOutput("SecretsList", secretsJSONlist);
     } catch (error) {
